c4ab719c08283ac0e910c9a115a8a0bf4ad86108
Yutaka Tsuzuki <yutaka.tsuzuki@ipmu.jp>
2020-12-31 15:29:28 +0900

    more complicated layers

--- gan_test.py
+++ gan_test.py
@@ -5,9 +5,10 @@ import numpy as np
 import pandas as pd
 
 
-SIZE_BATCH = 32
-SIZE_HALF_BATCH = 16
-N_EPOCH = 5000
+N_DIM_RANDOM = 64
+SIZE_BATCH = 128
+SIZE_HALF_BATCH = 64
+N_EPOCH = 1000
 
 
 # train and test
@@ -23,7 +24,7 @@ def load_train_test() :
 	return (x_train, y_train), (x_test, y_test)
 
 # model
-def build_generator(dim_noise = 256, shape_image = [28, 28]) :
+def build_generator(dim_noise = N_DIM_RANDOM, shape_image = [28, 28]) :
 	model = keras.models.Sequential()
 	model.add(keras.layers.Dense(64 * np.prod(shape_image) / 16, input_dim = dim_noise))
 	model.add(keras.layers.LeakyReLU(alpha = 0.2))
@@ -43,7 +44,7 @@ def build_discriminator(shape_image = [28, 28]) :
 	model.add(keras.layers.Conv2D(32, (3, 3), input_shape = (shape_image[0], shape_image[1], 1)))
 	model.add(keras.layers.LeakyReLU(alpha = 0.2))
 	model.add(keras.layers.Flatten())
-	model.add(keras.layers.Dense(128))
+	model.add(keras.layers.Dense(512))
 	model.add(keras.layers.LeakyReLU(alpha = 0.2))
 	model.add(keras.layers.Dense(64))
 	model.add(keras.layers.LeakyReLU(alpha = 0.2))
@@ -73,7 +74,7 @@ def main() :
 	for i_epoch in range(N_EPOCH) :
 		# train discriminator
 		# generate train data
-		xx_noise = np.random.normal(0.0, 1.0, (SIZE_HALF_BATCH, 256))
+		xx_noise = np.random.normal(0.0, 1.0, (SIZE_HALF_BATCH, N_DIM_RANDOM))
 		x_train_gen = generator.predict(xx_noise)
 		idx_train_real = np.random.randint(0, len(x_train), SIZE_HALF_BATCH)
 		x_train_real = x_train[idx_train_real]
@@ -82,13 +83,13 @@ def main() :
 		loss_discr_real = np.array( discriminator.train_on_batch(x_train_real, np.zeros((SIZE_HALF_BATCH, 1))) )
 		loss_discr = 0.5 * (loss_discr_gen + loss_discr_real)
 		# train generator
-		xx_noise_2 = np.random.normal(0.0, 1.0, (SIZE_BATCH, 256))
+		xx_noise_2 = np.random.normal(0.0, 1.0, (SIZE_BATCH, N_DIM_RANDOM))
 		y_train_real = np.array([1] * SIZE_BATCH)
 		loss_gener = joint.train_on_batch(xx_noise_2, y_train_real)
 		# print
 		print("%d / %d : Discr %f, Gener %f" % (i_epoch + 1, N_EPOCH, loss_discr[0], loss_gener[0]))
 		# save image
-		xx_noise_3 = np.random.normal(0.0, 1.0, (16, 256))
+		xx_noise_3 = np.random.normal(0.0, 1.0, (16, N_DIM_RANDOM))
 		x_train_gen_3 = generator.predict(xx_noise_3)
 		if (i_epoch + 1) % 100 == 0 :
 			fname_out = "image_%04d_gan_test.csv" % (i_epoch + 1)
