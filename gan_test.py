22c34730236b78917f5772c7a43892f932535b3f
Yutaka Tsuzuki <yutaka.tsuzuki@ipmu.jp>
2021-01-01 14:36:17 +0900

    modify data structure

--- gan_test.py
+++ gan_test.py
@@ -6,14 +6,14 @@ import pandas as pd
 
 
 N_DIM_RANDOM = 128
-SIZE_BATCH = 32
-SIZE_HALF_BATCH = 16
+SIZE_BATCH = 64
+SIZE_HALF_BATCH = 32
 N_EPOCH = 5000
 
 FACTOR_TOLERANCE = 3.0
 FACTOR_HYSTERESIS = 1.25
-LR_D = 0.0002
-LR_G = 0.0005
+LR_D = 0.001
+LR_G = 0.001
 
 
 # train and test
@@ -22,8 +22,10 @@ def load_train_test() :
 	(x_train, y_train), (x_test, y_test) = mnist.load_data()
 	print(x_train.shape)
 	print(y_train.shape)
-	x_train = 2.0 * x_train / np.max(x_train) - 1.0
-	x_test = 2.0 * x_test / np.max(x_test) - 1.0
+	#x_train = 2.0 * x_train / np.max(x_train) - 1.0
+	#x_test = 2.0 * x_test / np.max(x_test) - 1.0
+	x_train = x_train / np.max(x_train)
+	x_test = x_test / np.max(x_test)
 	x_train = x_train.reshape(len(x_train), 28, 28, 1)
 	x_test = x_test.reshape(len(x_test), 28, 28, 1)
 	return (x_train, y_train), (x_test, y_test)
@@ -31,29 +33,29 @@ def load_train_test() :
 # model
 def build_generator(dim_noise = N_DIM_RANDOM, shape_image = [28, 28]) :
 	model = keras.models.Sequential()
-	model.add(keras.layers.Dense(128 * np.prod(shape_image) / 16, input_dim = dim_noise))
-	model.add(keras.layers.LeakyReLU(alpha = 0.2))
-	model.add(keras.layers.Reshape((int(shape_image[0] / 4), int(shape_image[1] / 4), 128)))
-	model.add(keras.layers.BatchNormalization(momentum = 0.8))
+	model.add(keras.layers.Dense(256 * np.prod(shape_image) / 16, input_dim = dim_noise, activation = "tanh"))
+	#model.add(keras.layers.LeakyReLU(alpha = 0.2))
+	model.add(keras.layers.Reshape((int(shape_image[0] / 4), int(shape_image[1] / 4), 256)))
+	#model.add(keras.layers.BatchNormalization(momentum = 0.8))
 	model.add(keras.layers.UpSampling2D((2, 2)))
-	model.add(keras.layers.Conv2D(64, (3, 3), padding = "same"))
-	model.add(keras.layers.LeakyReLU(alpha = 0.2))
+	model.add(keras.layers.Conv2D(64, (3, 3), padding = "same", activation = "tanh"))
+	#model.add(keras.layers.LeakyReLU(alpha = 0.2))
 	model.add(keras.layers.UpSampling2D((2, 2)))
-	model.add(keras.layers.Conv2D(1, (3, 3), padding = "same", activation = "tanh"))
+	model.add(keras.layers.Conv2D(1, (3, 3), padding = "same", activation = "sigmoid"))
 	model.add(keras.layers.Reshape((shape_image[0], shape_image[1], 1)))
 	return model
 
 def build_discriminator(shape_image = [28, 28]) :
 	model = keras.models.Sequential()
-	model.add(keras.layers.Conv2D(16, (3, 3), input_shape = (shape_image[0], shape_image[1], 1)))
+	model.add(keras.layers.Conv2D(64, (3, 3), input_shape = (shape_image[0], shape_image[1], 1)))
 	model.add(keras.layers.LeakyReLU(alpha = 0.2))
 	model.add(keras.layers.MaxPooling2D(pool_size = (2, 2)))
-	#model.add(keras.layers.Conv2D(64, (3, 3)))
-	#model.add(keras.layers.LeakyReLU(alpha = 0.2))
-	#model.add(keras.layers.MaxPooling2D(pool_size = (2, 2)))
-	model.add(keras.layers.Flatten())
-	model.add(keras.layers.Dense(512))
+	model.add(keras.layers.Conv2D(256, (3, 3)))
 	model.add(keras.layers.LeakyReLU(alpha = 0.2))
+	model.add(keras.layers.MaxPooling2D(pool_size = (2, 2)))
+	model.add(keras.layers.Flatten())
+	#model.add(keras.layers.Dense(512))
+	#model.add(keras.layers.LeakyReLU(alpha = 0.2))
 	model.add(keras.layers.Dense(64))
 	model.add(keras.layers.LeakyReLU(alpha = 0.2))
 	model.add(keras.layers.Dense(1, activation = "sigmoid"))
